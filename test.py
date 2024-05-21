0fb71642beac71f74ee4817ac4181feb6874f080
Freedomity <113837486+Puchit-Bunpleng@users.noreply.github.com>
2024-05-21 15:41:47 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from glob import glob
-import os
-
-files = []
-for folder in glob('src/models/*'):
-    for file in glob(os.path.join(folder,'*')):
-        files.append((file, file))
-print(files)
